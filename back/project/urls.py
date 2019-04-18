14298564cb6dcce3d82e42595f7fa88a0691d9ec
Victor Villalobos <victorvillalobos@outlook.com>
2019-04-17 21:44:27 -0400

    refator to more human structure

--- back/project/urls.py
+++ back/project/urls.py
@@ -13,11 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+
 from django.contrib import admin
+from django.urls import path
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^$', TemplateView.as_view(template_name='project/spa.html'), name='home'),
+  path('admin/', admin.site.urls),
+  path('', TemplateView.as_view(template_name='project/spa.html'), name='home'),
 ]
